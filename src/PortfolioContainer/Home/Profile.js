9b71f9744dbbf53dc9be832076d77710c38ccc69
Tandid Alam <tandid.alam@gmail.com>
2021-11-09 01:25:28 -0500

    setup links for github and linkedin

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a
+              href="https://www.linkedin.com/in/tandidalam/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+            <a
+              href="https://github.com/Tandid"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="fa fa-github-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
