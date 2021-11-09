858676d47b5ae2779f4d7f98d0f60b3e836b24b6
Tandid Alam <tandid.alam@gmail.com>
2021-11-09 01:52:20 -0500

    added react-typical for typing animation

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
   return (
@@ -20,6 +21,28 @@ export default function Profile() {
             >
               <i className="fa fa-github-square"></i>
             </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text"> Tandid </span>{" "}
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev",
+                    1000,
+                    "Fullstack Developer",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
           </div>
         </div>
       </div>
