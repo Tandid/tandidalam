9e78a826dbba1fb028f54b72e70df0ca1ef63610
Tandid Alam <tandid.alam@gmail.com>
2021-11-10 01:23:29 -0500

    added hamburger to header

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -19,5 +19,60 @@ export default function Header() {
   };
   let currentScreenSubscription =
     ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
-  return <div></div>;
+
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option";
+    if (index < TOTAL_SCREENS.lenght - 1) classes += "header-option-seperator";
+
+    if (selectedScreen === index) classes += "selected-header-option";
+    return;
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementByI(screen.screen_name);
+    if (!screenComponent) return;
+
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+  return (
+    <div>
+      <div
+        className="header-option"
+        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+      >
+        <div className="header-parent">
+          <div
+            className="header-hamburger"
+            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+          >
+            <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+          </div>
+          <div className="header-logo">
+            <span> Tandid </span>
+          </div>
+          <div
+            className={
+              showHeaderOptions
+                ? "header-options show-hamburger-options"
+                : "header-options"
+            }
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 }
