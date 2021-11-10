b364e55cef049a0c2b41b51478ab6330eede2961
Tandid Alam <tandid.alam@gmail.com>
2021-11-10 01:29:02 -0500

    minor bug fixes

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TOTAL_SCREENS,
   GET_SCREEN_INDEX,
 } from "../../../utilities/commonUtils";
-import ScrollService from "../../../utilities/scrollService";
+import ScrollService from "../../../utilities/ScrollService";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Header.css";
