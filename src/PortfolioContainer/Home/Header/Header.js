2c94f229deb30bf5a4bc02bce108092c36cf454e
Tandid Alam <tandid.alam@gmail.com>
2021-11-10 00:59:59 -0500

    header added and package installation

---
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  TOTAL_SCREENS,
+  GET_SCREEN_INDEX,
+} from "../../../utilities/commonUtils";
+import ScrollService from "../../../utilities/scrollService";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
+
+export default function Header() {
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+  const updateCurrentScreen = (currentScreen) => {
+    if (!currentScreen || !currentScreen.screenInView) return;
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+    if (screenIndex < 0) return;
+  };
+  let currentScreenSubscription =
+    ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+  return <div></div>;
+}
