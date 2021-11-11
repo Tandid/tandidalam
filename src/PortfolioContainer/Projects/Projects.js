8200f9674db8b6b732671ffb74f79d1bee8c1f29
Tandid Alam <tandid.alam@gmail.com>
2021-11-10 20:34:44 -0500

    added react-flippy, flip card featuer

--- src/PortfolioContainer/Projects/Projects.js
+++ src/PortfolioContainer/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 import "./Projects.css";
 
 export default function Projects(props) {
@@ -21,85 +22,151 @@ export default function Projects(props) {
         <ScreenHeading title={"Projects"} />
 
         {/* Project 1 */}
-
-        <div className="project-card">
-          <div className="project-1"></div>
-          <div className="overlay overlayBottom">
-            <div className="overlay-content">
-              <a
-                className="live-demo-button"
-                href="https://poptoons-shop.herokuapp.com/ "
-                rel="noreferrer"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-              <a
-                className="github-button"
-                href="https://github.com/Tandid/Poptoons-Shop"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i className="fa fa-github"></i>
-                Github
-              </a>
+        <Flippy
+          flipOnHover={false} // default false
+          flipOnClick={true} // default false
+          flipDirection="horizontal"
+        >
+          <FrontSide className="project-front-card">
+            <div className="project-1"></div>
+            <div className="overlay overlayBottom">
+              <div className="overlay-content">
+                <a
+                  className="live-demo-button"
+                  href="https://poptoons-shop.herokuapp.com/ "
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="github-button"
+                  href="https://github.com/Tandid/Poptoons-Shop"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github"></i>
+                  Github
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
+          </FrontSide>
+          <BackSide className="project-back-card">
+            <div className="project-name"> Pop Toons Shop </div>
+            <div className="project-description">
+              Poptoons Shop is an E-commerce site that sells pop vinyl figurines
+              also known as Funko Pops. This E-commerce site offers a wide
+              selection of Funko Pops from popular films, videogames, sitcoms,
+              and anime series. In the site users are able to purchase products,
+              go through a checkout system, view their order history, and if
+              they have admin access, they're able to add/edit products, orders,
+              and user data.
+            </div>
+            <div className="project-tech-stack">
+              <div className="tech-stack-title">Implemented Technology</div>
+              <div className="tech-stack-list">
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+              </div>
+            </div>
+          </BackSide>
+        </Flippy>
 
         {/* Project 2 */}
-
-        <div className="project-card">
-          <div className="project-2"></div>
-          <div className="overlay overlayBottom">
-            <div className="overlay-content">
-              <a
-                className="live-demo-button"
-                href="https://quacker-twitterclone.herokuapp.com/ "
-                rel="noreferrer"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-              <a
-                className="github-button"
-                href="https://github.com/Tandid/Quacker-TwitterClone"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i className="fa fa-github"></i>
-                Github
-              </a>
+        <Flippy
+          flipOnHover={false} // default false
+          flipOnClick={true} // default false
+          flipDirection="horizontal"
+        >
+          <FrontSide className="project-front-card">
+            <div className="project-2"></div>
+            <div className="overlay overlayBottom">
+              <div className="overlay-content">
+                <a
+                  className="live-demo-button"
+                  href="https://quacker-twitterclone.herokuapp.com/ "
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="github-button"
+                  href="https://github.com/Tandid/Quacker-TwitterClone"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github"></i>
+                  Github
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
+          </FrontSide>
+          <BackSide className="project-back-card">
+            <div className="project-name"> Quacker </div>
+            <div className="project-description">
+              Quacker is a social media app that mimics Twitter's layouts and
+              features. Users can check out the latest news, follow others, and
+              chat with eachother real time!
+            </div>
+            <div className="project-tech-stack">
+              <div className="tech-stack-title">Implemented Technology</div>
+              <div className="tech-stack-list">
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+              </div>
+            </div>
+          </BackSide>
+        </Flippy>
 
         {/* Project 3 */}
-
-        <div className="project-card">
-          <div className="project-3"></div>
-          <div className="overlay overlayBottom">
-            <div className="overlay-content">
-              <a
-                className="live-demo-button"
-                href="/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-              <a
-                className="github-button"
-                href="/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <i className="fa fa-github"></i>
-                Github
-              </a>
+        <Flippy
+          flipOnHover={false} // default false
+          flipOnClick={true} // default false
+          flipDirection="horizontal"
+        >
+          <FrontSide className="project-front-card">
+            <div className="project-3"></div>
+            <div className="overlay overlayBottom">
+              <div className="overlay-content">
+                <a
+                  className="live-demo-button"
+                  href="/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="github-button"
+                  href="/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github"></i>
+                  Github
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
+          </FrontSide>
+          <BackSide className="project-back-card">
+            <div className="project-name"> Lorem Ipsum </div>
+            <div className="project-description"></div>
+            <div className="project-tech-stack">
+              <div className="tech-stack-title">Implemented Technology</div>
+              <div className="tech-stack-list">
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+                <div>Icon</div>
+              </div>
+            </div>
+          </BackSide>
+        </Flippy>
       </div>
     </div>
   );
