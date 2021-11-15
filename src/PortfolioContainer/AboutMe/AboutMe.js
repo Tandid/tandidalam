330f40775599a57398408dcef2d0fa2ac80ed1c5
Tandid Alam <tandid.alam@gmail.com>
2021-11-14 22:28:45 -0500

    updated file name

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-import ScrollService from "../../utilities/ScrollService";
+import ScrollService from "../../utilities/ScrollService.js";
 import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
